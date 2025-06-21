3556473a8c8f814b748b43696aa5027d19b89cbd
Preslav977 <p.cvetanow@gmail.com>
2025-06-21 14:36:01 +0300

    Created thunk for deleting a recipe with every minimal interface.

---
+++ src/thunks/recipeThunks/deleteRecipeThunk.ts
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { deleteRecipe } from "../../firebaseConfig/firebaseconfig";
+
+export const deleteRecipeThunk = createAsyncThunk(
+  "recipe/deleteRecipe",
+  async (recipeId: string) => {
+    const deleteRecipeById = await deleteRecipe(recipeId);
+
+    return deleteRecipeById;
+  },
+);
+
+interface DeletedRecipe {
+  loading?: "idle" | "pending" | "succeeded" | "failed";
+  error: string;
+}
+
+const initialState: DeletedRecipe = {
+  loading: "idle",
+  error: "",
+};
+
+export const deleteRecipeSlice = createSlice({
+  name: "recipe/deleteRecipe",
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(deleteRecipeThunk.fulfilled, (state, action) => {
+      state.loading = "succeeded";
+    });
+
+    builder.addCase(deleteRecipeThunk.rejected, (state, action) => {
+      state.loading = "failed";
+    });
+  },
+});
