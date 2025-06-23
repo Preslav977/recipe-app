0efdc7962ee366d60bedde7acbab8ef4b8eae98f
Nikolay K. <Nikolaykosakov@gmail.com>
2025-06-24 01:01:28 +0200

    fix(auth): litle clean up  login and signup pages

---
+++ src/components/Pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+import Layout from "../Footer/Footer";
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+function LoginPage() {
+  return <LoginForm />;
+}
+
+export default LoginPage;
---
+++ src/components/Pages/SignupPage.tsx
@@ -0,0 +1,8 @@
+import SignUpForm from "../SignUpForm/SignUpForm";
+import Layout from "../Footer/Footer";
+
+function SignupPage() {
+  return <SignUpForm />;
+}
+
+export default SignupPage;
