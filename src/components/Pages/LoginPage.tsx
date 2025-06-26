482113a09160c50ee5eb0bd4289da7c94d172eed
Nikolay K. <Nikolaykosakov@gmail.com>
2025-06-26 09:57:34 +0200

    Deleted non used components

--- src/components/Pages/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from "../Footer/Footer";
-import LoginForm from "../../components/LoginForm/LoginForm";
-
-function LoginPage() {
-  return <LoginForm />;
-}
-
-export default LoginPage;
