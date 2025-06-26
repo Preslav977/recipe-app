fe901ba5cc8237dd7350d0136c90269585c4acb3
Nikolay K. <Nikolaykosakov@gmail.com>
2025-06-26 09:57:00 +0200

    Fixed some code dublication and malfunction due to a bad merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-
-import './App.css'
-import React from "react"; 
-import Navbar from "./components/Navigation/Navbar";
- 
-import SignUpForm from "./components/SignUpForm/SignUpForm";
-import LoginForm from "./components/LoginForm/LoginForm";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import FullScreenLoader from './components/Loader/FullScreenLoader';
-
-// import { BrowserRouter as Router } from "react-router-dom";
-
 // import './App.css'
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navigation/Navbar";
 
 function App() {
-  const Navigate = useNavigate();
   return (
     <Provider store={store}>
-      <Navbar />         
+      <Navbar />
       <Outlet />
       <Footer />
     </Provider>
-    
   );
 }
 export default App;
