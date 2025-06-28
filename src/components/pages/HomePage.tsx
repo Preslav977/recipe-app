5d31f442d7adc269d1a29df0a1ff9214a70b17c9
Preslav977 <p.cvetanow@gmail.com>
2025-06-28 10:34:24 +0300

    Reused Recipe card component to show the user recipes, and added some CSS styling to the dashboad boxes

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Paper, Button } from "@mui/material";
+import { Box, Typography, Paper, Button, Container } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store/store";
@@ -9,6 +9,8 @@ import { emailVerification } from "../../firebaseConfig/firebaseconfig";
 import { useNavigate } from "react-router-dom";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import { SvgIcon } from "@mui/material";
+import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
+import { RecipeCard } from "../RecipeCard/RecipeCard";
 
 export const HomePage = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -27,7 +29,7 @@ export const HomePage = () => {
     dispatch(getUserFavoriteRecipeListThunk(`${uid}`));
   }, [dispatch, uid]);
 
-  const { recipes } = useSelector(
+  const { recipes, loading, error } = useSelector(
     (state: RootState) => state.getAllRecipesThunk,
   );
 
@@ -50,118 +52,192 @@ export const HomePage = () => {
   const myRecipes = recipes.filter((recipe) => recipe.authorId === uid);
 
   return (
-    <Box
-      sx={{
-        marginTop: "0.5em",
-        padding: "1.5em",
-      }}
-    >
-      <Typography variant="h2" fontSize={"2rem"}>
-        Dashboard
-      </Typography>
-      <Box sx={{ display: "flex", gap: "1em", flexWrap: "wrap" }}>
-        <Paper
-          sx={{
-            maxWidth: 250,
-            backgroundColor: "#1976d2",
-            width: "100%",
-            padding: "1rem",
-            marginTop: "0.5em",
-            color: "white",
-          }}
-        >
-          {newFilteredAllRecipesArray.length}
-          <Typography variant="body2">All Recipes</Typography>
-          <Link
-            style={{
-              textDecoration: "none",
-              color: "white",
-              display: "flex",
-              gap: "0.5em",
-            }}
-            to="/recipes"
-          >
-            More information
-            <SvgIcon component={ArrowCircleRightIcon} />
-          </Link>
-        </Paper>
-
-        <Paper
-          sx={{
-            maxWidth: 250,
-            backgroundColor: "#1976d2",
-            width: "100%",
-            padding: "1rem",
-            marginTop: "0.5em",
-            color: "white",
-          }}
-        >
-          {myRecipes.length}
-          <Typography variant="body2">My Recipes</Typography>
-          <Link
-            style={{
-              textDecoration: "none",
-              color: "white",
-              display: "flex",
-              gap: "0.5em",
-            }}
-            to="/recipes"
-          >
-            More information
-            <SvgIcon component={ArrowCircleRightIcon} />
-          </Link>
-        </Paper>
-
-        <Paper
-          sx={{
-            backgroundColor: "#1976d2",
-            maxWidth: 250,
-            width: "100%",
-            padding: "1rem",
-            marginTop: "0.5em",
-            color: "white",
-          }}
-        >
-          {favoriteRecipes.length}
-          <Typography variant="body2">Favorite Recipes</Typography>
-          <Link
-            style={{
-              textDecoration: "none",
-              color: "white",
-              display: "flex",
-              gap: "0.5em",
-            }}
-            to="/favoriteRecipes"
-          >
-            More information
-            <SvgIcon component={ArrowCircleRightIcon} />
-          </Link>
-        </Paper>
-      </Box>
-
-      {!emailVerified ? (
+    <>
+      <Box
+        sx={{
+          marginTop: "0.5em",
+          padding: "1.5em",
+        }}
+      >
+        <Typography variant="h2" fontSize={"2rem"}>
+          Dashboard
+        </Typography>
         <Box
           sx={{
-            marginTop: "0.5em",
+            display: "flex",
+            gap: "1em",
+            flexWrap: "wrap",
+            padding: "2em",
+            "@media (max-width: 650px)": {
+              padding: 0,
+            },
           }}
         >
-          <Typography variant="body2" sx={{ fontSize: "1.25rem" }}>
-            Note: Your email is not verified, you can only read!
-          </Typography>
-          <Button
-            onClick={() => {
-              emailVerification();
-
-              setTimeout(() => navigate("/login"), 2000);
+          <Paper
+            sx={{
+              maxWidth: 350,
+              height: 125,
+              backgroundColor: "#1976d2",
+              width: "100%",
+              padding: "1rem",
+              marginTop: "0.5em",
+              color: "white",
             }}
-            sx={{ marginLeft: "-9.4px" }}
           >
-            Click here to send email link!
-          </Button>
+            {newFilteredAllRecipesArray.length}
+            <Typography variant="body2">All Recipes</Typography>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "white",
+                display: "flex",
+                gap: "0.5em",
+              }}
+              to="/recipes"
+            >
+              More information
+              <SvgIcon component={ArrowCircleRightIcon} />
+            </Link>
+          </Paper>
+
+          <Paper
+            sx={{
+              maxWidth: 350,
+              height: 125,
+              backgroundColor: "#1976d2",
+              width: "100%",
+              padding: "1rem",
+              marginTop: "0.5em",
+              color: "white",
+            }}
+          >
+            {myRecipes.length}
+            <Typography variant="body2">My Recipes</Typography>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "white",
+                display: "flex",
+                gap: "0.5em",
+              }}
+              to="/recipes"
+            >
+              More information
+              <SvgIcon component={ArrowCircleRightIcon} />
+            </Link>
+          </Paper>
+
+          <Paper
+            sx={{
+              backgroundColor: "#1976d2",
+              maxWidth: 350,
+              height: 125,
+              width: "100%",
+              padding: "1rem",
+              marginTop: "0.5em",
+              color: "white",
+            }}
+          >
+            {favoriteRecipes.length}
+            <Typography variant="body2">Favorite Recipes</Typography>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "white",
+                display: "flex",
+                gap: "0.5em",
+              }}
+              to="/favoriteRecipes"
+            >
+              More information
+              <SvgIcon component={ArrowCircleRightIcon} />
+            </Link>
+          </Paper>
         </Box>
-      ) : (
-        ""
-      )}
-    </Box>
+
+        {!emailVerified ? (
+          <Box
+            sx={{
+              marginTop: "0.5em",
+            }}
+          >
+            <Typography variant="body2" sx={{ fontSize: "1.25rem" }}>
+              Note: Your email is not verified, you can only read!
+            </Typography>
+            <Button
+              onClick={() => {
+                emailVerification();
+
+                setTimeout(() => navigate("/login"), 2000);
+              }}
+              sx={{ marginLeft: "-9.4px" }}
+            >
+              Click here to send email link!
+            </Button>
+          </Box>
+        ) : (
+          ""
+        )}
+      </Box>
+      <Box
+        sx={{
+          marginTop: "0.5em",
+          padding: "1.5em",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1em",
+            flexWrap: "wrap",
+          }}
+        >
+          <Typography
+            gutterBottom
+            variant="h2"
+            fontSize={"2rem"}
+            sx={{ mt: 2 }}
+          >
+            My Recipes
+          </Typography>
+          {loading === "pending" && <LoadingSpinner />}
+          {loading === "failed" && (
+            <Typography
+              sx={{
+                color: "red",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              Error: {error}
+            </Typography>
+          )}
+          {myRecipes.length === 0 && (
+            <Typography
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              No Recipes Yet
+            </Typography>
+          )}
+
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+            }}
+          >
+            {myRecipes.map((recipe, index) => (
+              <RecipeCard key={index} recipe={recipe} path="recipe" />
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
 };
